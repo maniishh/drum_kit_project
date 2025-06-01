10818417cc1c4eed9f138701cc96f7c643bb8201
Manish Kumar Yadav <162919290+maniishh@users.noreply.github.com>
2025-06-01 12:22:28 +0530

    Update index.js

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -2,7 +2,18 @@
   for (var i=0;i<number;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function(){
         var ButtonInnerHtml =this.innerHTML;
-        switch(ButtonInnerHtml){
+        makeSound(ButtonInnerHtml);
+        
+    });
+  }
+document.addEventListener("keypress",function(event){
+    makeSound(event.key);
+});
+
+
+
+  function makeSound(key){
+    switch(key){
             case "w":
                 var tom1= new Audio("sounds/tom-1.mp3");
                 tom1.play();
@@ -35,5 +46,4 @@
                 default:
                     console.log("ButtonInnerHtml");
         }
-    });
   }
